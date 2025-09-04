aaf2a252b8afa180719c3515569e6816f8447378
Vivek Sahu <viveksahu1762@gmail.com>
2025-09-04 16:15:05 +0530

    chore(test): fix remaining single quotes and format

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,8 @@
-import { createStealthAddress } from 'unwallet-sdk';
+import { createStealthAddress } from "unwallet-sdk";
 
 // generate stealth address for base sepolia
 createStealthAddress({
-  username: 'kyskkysk',
+  username: "kyskkysk",
   chainId: 84532,
   tokenAddress: "0x036cbd53842c5426634e7929541ec2318f3dcf7e",
 }).then((address) => {
